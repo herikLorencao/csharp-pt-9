3f959cb75b94470d67541ba198bd931c7759c612
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-23 14:05:18 -0300

    adicionando leitura de arquivos

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,18 +1,34 @@
-﻿using ByteBankImportacaoExportacao.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
+            var fileStream = new FileStream("contas.txt", FileMode.Open);
+            var buffer = new byte[1024];
+            var quantidadeBytesLidos = -1;
+
+            while (quantidadeBytesLidos != 0)
+            {
+                quantidadeBytesLidos = fileStream.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fileStream.Read(buffer, 0, 1024);
+
             Console.ReadLine();
         }
+
+        private static void EscreverBuffer(byte[] buffer)
+        {
+            var encoding = Encoding.UTF8;
+            var texto = encoding.GetString(buffer);
+
+            Console.Write(texto);
+        }
     }
-} 
- 
+}
