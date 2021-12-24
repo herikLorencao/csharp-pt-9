cad02279a0dcf9d4913666d97969783758cbb987
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-24 09:10:24 -0300

    adicionando métodos auxiliares

--- ByteBankImportacaoExportacao/MetodosAuxiliares.cs
+++ ByteBankImportacaoExportacao/MetodosAuxiliares.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,14 @@ namespace ByteBankImportacaoExportacao
     {
         private static void LeituraEscritaAuxiliares()
         {
+            File.WriteAllText("auxiliar.txt", "Conteúdo completo do arquivo");
+            Console.WriteLine("Escrita do arquivo auxiliar.txt realizada!");
 
+            var bytes = File.ReadAllBytes("binario.txt");
+            Console.WriteLine($"Arquivo contas possui: {bytes.Length} bytes");
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
         }
     }
 }
